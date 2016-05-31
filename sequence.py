47321ba0e02da1f1c3bb7fa699e31b40ae06e7f9
Hazelfire <samnolan555@gmail.com>
2016-05-31 20:46:02 +1000

    Added numbersUnderWhich
    similar to numberUnderThat, but all of the requirements need to be furfilled for the number to be included in the set

--- sequence.py
+++ sequence.py
@@ -1,4 +1,4 @@
-# Returns the numbers below a cap that follow a requirement
+# Returns the numbers below a cap that follow any of the requirements
 def numbersUnderThat(number, requirements):
     i = 0
     numberSet = []
@@ -7,5 +7,21 @@ def numbersUnderThat(number, requirements):
             if requirement(i):
                 numberSet.append(i)
                 break
+        i+=1
+    return numberSet
+
+# Returns the numbers below a cap that follow all of the requirements
+def numbersUnderWhich(number, requirements):
+    i = 0
+    numberSet = []
+    while number > i:
+        allRequirementsFurfilled = True
+        for requirement in requirements:
+            if not requirement(i):
+                allRequirementsFurfilled = False
+                break
+
+        if allRequirementsFurfilled:
+            numberSet.append(i)
         i+=1
     return numberSet
