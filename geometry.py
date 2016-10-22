40a57a568aa3865a4026c5f135435769d3e49c9b
Ewan Breakey <ebreakey@gmail.com>
2016-10-22 20:21:47 +1100

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -5,6 +5,9 @@ import math
 def ps_area(w, h):
     return (w * h)
 
+def point_distance(x1, y1, x2, y2):
+    return math.sqrt(math.pow(y2-y1), math.pow(x2-x1))
+
 def aabb_area(x1, y1, x2, y2):
     return ps_area(y2-y1, x2-x1)
 
